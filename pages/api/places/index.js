437d6bf9d30a828a4d1a1e394bca86f28812297b
Adam Harris <adamtjharris@gmail.com>
2025-04-25 12:27:30 +0200

    remove brackets

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -1,5 +1,6 @@
 import dbConnect from "@/lib/_db_Connect";
 import Place from "@/db/models/tourio";
+
 export default async function handler(request, response) {
   await dbConnect();
   if (request.method === "GET") {
