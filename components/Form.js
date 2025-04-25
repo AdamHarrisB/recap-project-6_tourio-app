a1f6355cd87caba4bde6239204b9c1d0ddb72e6e
Adam Harris <adamtjharris@gmail.com>
2025-04-25 13:13:00 +0200

    update

--- components/Form.js
+++ components/Form.js
@@ -1,41 +1,52 @@
 import styled from "styled-components";
 import { StyledButton } from "./StyledButton";
-
+import useSWR from "swr";
 export const FormContainer = styled.form`
   display: grid;
   gap: 0.5rem;
 `;
-
 export const Input = styled.input`
   padding: 0.5rem;
   font-size: inherit;
   border: 1px solid black;
   border-radius: 0.5rem;
 `;
-
 export const Textarea = styled.textarea`
   font-family: inherit;
   border: 1px solid black;
   border-radius: 0.5rem;
   padding: 0.5rem;
 `;
-
 export const Label = styled.label`
   font-weight: bold;
 `;
-
+//updating submit handler
 export default function Form({ onSubmit, formName, defaultData }) {
-  function handleSubmit(event) {
+  const {mutate}= useSWR("@/pages/api/places/index");
+  async function handleSubmit(event) {
     event.preventDefault();
+    const form  = event.target;
+    const response = await fetch ("@/pages/api/places/index",{
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(productData),
+    });
+    if (response.ok) {
+      mutate();
+    }
+    form.reset();
+    form.elements.name.focus();
     const formData = new FormData(event.target);
     const data = Object.fromEntries(formData);
     onSubmit(data);
   }
-
   return (
     <FormContainer aria-labelledby={formName} onSubmit={handleSubmit}>
       <Label htmlFor="name">Name</Label>
       <Input
+        required
         id="name"
         name="name"
         type="text"
@@ -43,6 +54,7 @@ export default function Form({ onSubmit, formName, defaultData }) {
       />
       <Label htmlFor="image-url">Image Url</Label>
       <Input
+      required
         id="image-url"
         name="image"
         type="text"
@@ -50,6 +62,7 @@ export default function Form({ onSubmit, formName, defaultData }) {
       />
       <Label htmlFor="location">Location</Label>
       <Input
+      required
         id="location"
         name="location"
         type="text"
@@ -57,6 +70,7 @@ export default function Form({ onSubmit, formName, defaultData }) {
       />
       <Label htmlFor="map-url">Map Url</Label>
       <Input
+      required
         id="map-url"
         name="mapURL"
         type="text"
@@ -64,6 +78,7 @@ export default function Form({ onSubmit, formName, defaultData }) {
       />
       <Label htmlFor="description">Description</Label>
       <Textarea
+      required
         name="description"
         id="description"
         cols="30"
@@ -76,3 +91,13 @@ export default function Form({ onSubmit, formName, defaultData }) {
     </FormContainer>
   );
 }
+
+
+
+
+
+
+
+
+
+
--- pages/create.js
+++ pages/create.js
@@ -3,17 +3,25 @@ import { useRouter } from "next/router";
 import Form from "../components/Form";
 import { StyledLink } from "../components/StyledLink";
 import useSWR from "swr";
-
+import dbConnect from "@/lib/_db_Connect";
+import Place from "@/db/models/tourio";
 const StyledBackLink = styled(StyledLink)`
   justify-self: flex-start;
 `;
-
 export default function CreatePlacePage() {
   const router = useRouter();
   async function addPlace(place) {
-    console.log("adding place");
+    // console.log("adding place");
+    if (requestAnimationFrame.method==="POST"){
+      try {const placeData = requestAnimationFrame.body;
+        await Place.create(placeData);
+        response.status(201).json({status:"Place Created"});
+      } catch (error){
+        console.log(error);
+        response.status(400).json({error:error.message});
+      }
+    }
   }
-
   return (
     <>
       <h2 id="add-place">Add Place</h2>
